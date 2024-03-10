b8ce34c299fc9d14bcf171e7bf48ef245a2ea2e4
AnNinh2605 <anninh1511@gmail.com>
2024-03-10 13:24:59 +0700

    <feat> create header & user list

--- src/component/TableUser.js
+++ src/component/TableUser.js
@@ -1,8 +1,47 @@
+import { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import Table from 'react-bootstrap/Table';
+import { fetchAllUser } from '../service/userService';
+
 const TableUser = () => {
+    const [listUser, setListUser] = useState('');
+    useEffect(() => {
+        //call api
+        getAlluser();
+    }, [])
+    const getAlluser = async () => {
+        let results = await fetchAllUser();
+        if (results && results.data && results.data.data) {
+            setListUser(results.data.data)
+        }
+    }
     return (
-        <>
-            TableUser
-        </>
+        <Container>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Email</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listUser && listUser.length > 0 ?
+                        listUser.map((item, index) => {
+                            return (
+                                <tr key={`userList-${index}`}>
+                                    <td>{item.id}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.first_name}</td>
+                                    <td>{item.last_name}</td>
+                                </tr>
+                            )
+                        }) : null
+                    }
+                </tbody>
+            </Table>
+        </Container>
     )
 }
 export default TableUser;
