f4ae9a7c56b871854497430e24ff92a0288048b4
AnNinh2605 <anninh1511@gmail.com>
2024-03-13 19:54:05 +0700

    <feat> login api

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,9 +1,24 @@
 import { useState } from 'react';
+import { loginApi } from '../service/userService'
+import { toast } from 'react-toastify';
 import '../App.scss'
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isShowPassword, setIsShowPassword] = useState(false);
+
+    const handleLoginButton = async () => {
+        if (!email || !password) {
+            toast.error("Missing email or password");
+        }
+        else {
+            let results = await loginApi(email, password);
+            if (results && results.token) {
+                localStorage.setItem("token", results.token);
+                toast.success("Login successful")
+            }
+        }
+    }
     return (
         <div className="login-container">
             <div className="m-auto col-10 col-sm-4 d-flex flex-column gap-2">
@@ -25,7 +40,9 @@ const Login = () => {
                     ></i>
                 </div>
                 <button className="btn btn-primary mt-2"
-                    disabled={email && password ? false : true}>Login</button>
+                    disabled={email && password ? false : true}
+                    onClick={() => handleLoginButton()}
+                >Login</button>
                 <div className='text-center'>
                     <i className="fa-solid fa-angles-left" role='button'></i>
                     <span role='button'> Go back</span>
--- src/service/userService.js
+++ src/service/userService.js
@@ -7,10 +7,13 @@ const createUser = (email, first_name, last_name) => {
     return axios.post(`/api/users`, { email, first_name, last_name });
 }
 const updateUser = (name, job) => {
-    return axios.put("/api/users/", { name, job });
+    return axios.put("/api/users", { name, job });
 }
 const deleteUser = (id) => {
     return axios.delete(`/api/users/${id}`);
 }
 
-export { fetchAllUser, createUser, updateUser, deleteUser };
+const loginApi = (email, password) => {
+    return axios.post(`/api/login`, {email, password});
+}
+export { fetchAllUser, createUser, updateUser, deleteUser, loginApi };
