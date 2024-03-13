503f3240da9706716a20b1c6e48232231e658407
AnNinh2605 <anninh1511@gmail.com>
2024-03-13 15:38:05 +0700

    <feat> design app layout

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import './App.scss';
 import Header from './component/Header';
 import TableUser from './component/TableUser';
 import { ToastContainer } from 'react-toastify';
-
+import Home from './component/Home';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <>
       <div className="app-container">
         <Header />
-        <TableUser />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user" element={<TableUser />} />
+        </Routes>
       </div>
       <ToastContainer
         position="top-center"
---
+++ src/component/Home.js
@@ -0,0 +1,51 @@
+const Home = () => {
+    return (
+        <div className="container">
+            <div className="mt-2">
+                <h5>Yêu cầu:</h5>
+                <ul>
+                    <li>
+                        Sử dụng API từ trang web <a href="https://reqres.in">https://reqres.in</a> để tạo website.
+                    </li>
+                    <li>
+                        Sử dụng framework Angular để tạo một màn hình website cơ bản bao gồm các chức năng:
+                        <ol className="d-flex flex-column gap-2 my-2">
+                            <li>Đăng nhập</li>
+                            <li>Thêm User</li>
+                            <li>Sửa User</li>
+                            <li>Xoá User</li>
+                            <li>Hiển thị tất cả các User</li>
+                            <li>Tìm kiếm User theo Id</li>
+                            <li>Sắp xếp theo FirstName</li>
+                            <li>Import User từ file .csv</li>
+                            <li>Export User ra file .csv</li>
+                        </ol>
+                    </li>
+                    <li>
+                        Tự do tùy chỉnh html, css, để có một website nhẹ nhàng, khoa học và đẹp.
+                    </li>
+                    <li>
+                        Commit và đẩy source code lên github public.
+                    </li>
+                    <li>
+                        Triển khai website lên <u>Heroku</u> để demo.
+                    </li>
+                </ul>
+                <h5>Result</h5>
+                <ul>
+                    <li>
+                        Thời gian hoàn thành: <span className="text-danger">1-3 ngày</span>
+                    </li>
+                    <li>
+                        Gửi link Heroku và Github link lại email này
+                    </li>
+                    <li>
+                        Thời gian phản hồi 2 ngày làm việc kể từ ngày nhận được bài thi.
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    )
+}
+export default Home;
